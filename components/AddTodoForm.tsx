af603734eddabc3ecf803721f6499114abbb954f
Alex Dingal <xelalagnid@gmail.com>
2023-11-04 19:34:29 +0800

    Add input checks and error text

--- components/AddTodoForm.tsx
+++ components/AddTodoForm.tsx
@@ -17,11 +17,17 @@ const initialState: Todo = {
 function AddTodoForm(props: AddTodoFormProps) {
   const { onAddTodo, currentTodo, isEdit } = props
   const [todo, setTodo] = useState<Todo>(initialState)
+  const [hasError, setHasError] = useState<boolean>(false)
 
   const onFormSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault()
-    onAddTodo(todo!)
-    setTodo(initialState)
+    if (todo.text !== '') {
+      onAddTodo(todo!)
+      setTodo(initialState)
+      setHasError(false)
+    } else {
+      setHasError(true)
+    }
   }
 
   const onInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
@@ -43,24 +49,31 @@ function AddTodoForm(props: AddTodoFormProps) {
   }, [currentTodo])
 
   return (
-    <form
-      onSubmit={onFormSubmit}
-      className="flex flex-row item-center justify-between mb-3 lg:mb-6"
-    >
-      <input
-        onChange={onInputChange}
-        className="w-full text-base text-gray-600 border border-e-0 border-gray-400 p-3 outline-none focus-within:border-gray-600 transition-colors rounded-s-md placeholder:text-xs"
-        type="text"
-        placeholder="Add new todo"
-        value={todo.text}
-      />
-      <button
-        className="bg-gray-500 text-sm md:text-base text-white font-semibold border-none border-gray-400 py-2 px-4 rounded-e-md cursor-pointer"
-        type="submit"
+    <div className="mb-3 lg:mb-6">
+      <form
+        onSubmit={onFormSubmit}
+        className="flex flex-row item-center justify-between"
       >
-        {isEdit ? 'Update' : 'Add'}
-      </button>
-    </form>
+        <input
+          onChange={onInputChange}
+          className="w-full text-base text-gray-600 border border-e-0 border-gray-400 p-3 outline-none focus-within:border-gray-600 transition-colors rounded-s-md placeholder:text-xs"
+          type="text"
+          placeholder="Add new todo"
+          value={todo.text}
+        />
+        <button
+          className="bg-gray-500 text-sm md:text-base text-white font-semibold border-none border-gray-400 py-2 px-4 rounded-e-md cursor-pointer"
+          type="submit"
+        >
+          {isEdit ? 'Update' : 'Add'}
+        </button>
+      </form>
+      {hasError && (
+        <p className="text-xs text-red-400 text-left">
+          * Input can't be empty.
+        </p>
+      )}
+    </div>
   )
 }
 
